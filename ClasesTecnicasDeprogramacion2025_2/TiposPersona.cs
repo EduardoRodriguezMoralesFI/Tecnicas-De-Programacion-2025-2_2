1bc9da914e8ce2c8a7374d4a63b63c2a9e79b019
Eduardo Rodriguez <eduardo.rodriguez.morales.999@gmail.com>
2025-02-18 22:54:44 -0600

    Se agrega la clase de tipos de personas

---
+++ ClasesTecnicasDeprogramacion2025_2/TiposPersona.cs
@@ -0,0 +1,54 @@
+﻿class Romantico : Persona
+{
+    //Constructor 
+    public Romantico(string nombre, int edad) : base (nombre, edad){ }
+
+    //Metodos Overside porque son heradodos de la clase Persona 
+    public override void MostrarInteres()
+    {
+        Console.WriteLine($"{Nombre} muestra su interes enviando una carta");
+    }
+
+    public override void ExpresarSentimientoss()
+    {
+        Console.WriteLine ($"{Nombre} expresa sus sentimientos dedicando un poema");
+    }
+
+}
+
+
+class Amistoso : Persona
+{
+    //Constructor 
+    public Amistoso(string nombre, int edad) : base(nombre, edad) { }
+
+    //Metodos Overside porque son heradodos de la clase Persona 
+    public override void MostrarInteres()
+    {
+        Console.WriteLine($"{Nombre} muestra su interes invitando un cafe");
+    }
+
+    public override void ExpresarSentimientoss()
+    {
+        Console.WriteLine($"{Nombre} expresa sus sentimientos compartiendo una historia divertida");
+    }
+
+}
+
+class Indiferente : Persona
+{
+    //Constructor 
+    public Indiferente(string nombre, int edad) : base(nombre, edad) { }
+
+    //Metodos Overside porque son heradodos de la clase Persona 
+    public override void MostrarInteres()
+    {
+        Console.WriteLine($"{Nombre} finge no mostrar su interes");
+    }
+
+    public override void ExpresarSentimientoss()
+    {
+        Console.WriteLine($"{Nombre} prefiere estar solo");
+    }
+
+}
