d6c50ab2d08adf9d1281740d8a5ad8b47875521e
Eduardo Rodriguez <eduardo.rodriguez.morales.999@gmail.com>
2025-02-19 00:46:15 -0600

    12:46am  19feb2025

--- ClasesTecnicasDeprogramacion2025_2/Persona.cs
+++ ClasesTecnicasDeprogramacion2025_2/Persona.cs
@@ -25,7 +25,23 @@
 
     //Sobrecarga de +
 
-    /*public static Persona operator +(Persona p1, Persona p2, Persona p3)
+    public static Persona operator +(Persona p1, Persona p2)
+    {
+        if (p1.GetType() == p2.GetType())
+        {
+            Console.WriteLine($"{p1.Nombre} & {p2.Nombre} son una pareja perfecta no pasaron solos el 14 de feb");
+
+        }
+        else
+        {
+            Console.WriteLine($"{p1.Nombre} & {p2.Nombre} son compatibles como amigos");
+        }
+
+        return new Persona($"{p1.Nombre} & {p2.Nombre} tienen un promedio de edad: ", p1.Edad + p2.Edad / 2);
+
+    }
+    /*
+    public static Persona operator +(Persona p1, Persona p2, Persona p3)
     {
         if (p1.GetType() == p2.GetType() && p1.GetType() == p3.GetType())
         {
--- ClasesTecnicasDeprogramacion2025_2/Program.cs
+++ ClasesTecnicasDeprogramacion2025_2/Program.cs
@@ -1,3 +1,36 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World! 8:35pm");
 
+Romantico juan = new Romantico("Juan", 25);
+Amistoso pedro = new Amistoso("Pedron", 30);
+Indiferente maria = new Indiferente("Maria", 20);
+Romantico ana = new Romantico("Ana", 22);
+Romantico paquita = new Romantico("Paquita", 26);
+
+
+juan.MostrarInteres();
+pedro.MostrarInteres();
+maria.MostrarInteres();
+ana.MostrarInteres();
+paquita.MostrarInteres();
+juan.ExpresarSentimientoss();
+pedro.ExpresarSentimientoss();
+maria.ExpresarSentimientoss();
+ana.ExpresarSentimientoss();
+paquita.ExpresarSentimientoss();
+
+//Formar parejas 
+
+Persona pareja1 = juan + pedro;
+Persona pareja2 = juan + ana;
+Console.WriteLine("-------------");
+Persona pareja3 = pareja1 + pareja2;
+
+
+
+//Compara pareja 
+/*
+Console.WriteLine($"Nueva pareja formada: {pareja1.Nombre} con edad pronedio : {pareja1.Edad}");
+Console.WriteLine($"Nueva pareja formada: {pareja2.Nombre} con edad pronedio : {pareja2.Edad}");
+
+*/
