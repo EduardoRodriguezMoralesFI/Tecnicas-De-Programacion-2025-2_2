e744c6292b3041df485aaee744d108f60b445afc
Eduardo Rodriguez <eduardo.rodriguez.er555@gmail.com>
2025-03-03 10:07:08 -0600

    10:03am03mar2025

---
+++ ClasesTecnicasDeprogramacion2025_2/Caballero.cs
@@ -0,0 +1,11 @@
+﻿class Caballero : Guerrero
+{
+    public Caballero(string nombre) : base(nombre, 120, 20) { }
+
+    public override void Atacar(Guerrero enemigo)
+    {
+        Console.WriteLine($"{Nombre} (caballero) usa golpe critico ");
+        base.Atacar( enemigo ); 
+    }
+
+}
--- ClasesTecnicasDeprogramacion2025_2/Program.cs
+++ ClasesTecnicasDeprogramacion2025_2/Program.cs
@@ -1,5 +1,44 @@
-﻿
+﻿static string ObtenerNombre()
+{
+    while (true)
+    {
+        try
+        {
+            Console.WriteLine("Ingresa el nombre de tu guerrero");
+            string nombre = Console.ReadLine()??"".Trim();
+            if(string.IsNullOrEmpty(nombre))
+            {
+                throw new ArgumentException("El nombre no puede esta vacio");
+            }
+            return nombre;
+        }
+        catch(Exception ex)
+        {
+            Console.WriteLine($"Error: {ex.Message}");
+        }
+    }
 
+}
 
+static int ObtenerOpcion()
+{
+    while (true)
+    {
+        try
+        {
+            Console.WriteLine("Ingresa la opcion: ");
+            int opcion = int.Parse(Console.ReadLine() ?? "");
+            if (opcion !=1 || opcion != 2|| opcion != 3 )
+            {
+                throw new ArgumentException($"opcion no valida");
+            }
+        }
+        catch
+        {
 
+        }
+    
+    }
+     
+}
 
